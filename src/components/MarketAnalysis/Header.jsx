0de2e0a3a7a14cd420d71c6285650e0153cbc104
jalejo <ja.alejo@gmail.com>
2023-11-13 13:04:33 -0300

    small update

--- src/components/MarketAnalysis/Header.jsx
+++ src/components/MarketAnalysis/Header.jsx
@@ -1,4 +1,11 @@
 import AdvanteIcons from "../AdvanteIcons";
+import DataCollectionImg from "../../images/market-analysis/data-collection-img.png"
+import MarketSegmentation from "../../images/market-analysis/market-segmentation.png"
+import IdentificationOpportunities from "../../images/market-analysis/identification-opportunities.png"
+import Implementation from "../../images/market-analysis/implementation.png"
+import ArrowStep1 from "../../images/market-analysis/arrow-step-1.svg"
+import ArrowStep2 from "../../images/market-analysis/arrow-step-2.svg"
+import ArrowStep3 from "../../images/market-analysis/arrow-step-3.svg"
 
 const Header = () => {
   return (
@@ -12,7 +19,61 @@ const Header = () => {
           <h2>Market analysis</h2>
         </div>
 
-        <div></div>
+        <div className="market-analysis-headline">
+          <h1>Keeping <span>you</span> <br />up to date</h1>
+
+          <div className="market-analysis-headline-copy">
+            <p>A comprehensive market analysis is the compass that guides successful decision-making, revealing the intricate details of the market's heartbeat. At Advante Digital, we specialize in delivering a meticulous blend of qualitative and quantitative insights through our Market Analysis services.</p>
+          </div>
+        </div>
+
+        <div className="image-steps">
+          <div className="data-collection-img">
+            <img 
+              src={ DataCollectionImg }
+              alt="Data Collection"
+            />
+            <div className="arrow-step-1">
+              <img 
+              src={ ArrowStep1 }
+              alt="Step 1"
+              />
+            </div>
+            <div className="market-segmentation-img">
+              <img 
+                src={ MarketSegmentation }
+                alt="Market Segmentation"
+              />
+              <div className="arrow-step-2">
+                <img 
+                src={ ArrowStep2 }
+                alt="Step 2"
+                />
+              </div>
+              <div className="identification-opportunities-img">
+                <img 
+                  src={ IdentificationOpportunities }
+                  alt="Market Segmentation"
+                />
+                <div className="arrow-step-3">
+                  <img 
+                  src={ ArrowStep3 }
+                  alt="Step 3"
+                  />
+                </div>
+                <div className="implementation-img">
+                  <img 
+                    src={ Implementation }
+                    alt="Implementation"
+                  />
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
